099ce326d027adb1505b200c2c13010a2f40be74
Tianxing Le <tianxing.le@shopee.com>
2020-12-11 19:08:51 +0800

    readv和writev的优化及错误场景

--- LinuxCIO/advancedIOSystemCall-Chapter3.c
+++ LinuxCIO/advancedIOSystemCall-Chapter3.c
@@ -23,3 +23,12 @@
 //段iovec的集合数组被称为向量（vector）
 //每个段中存了一个缓冲区（首字节的地址）和缓冲区的大小（字节为单位）
 //readv和writev会按顺序处理向量中的段（从iov[0]~iov[count-1]）
+
+//返回值：
+//1.分别返回读写的字节数。即count个iov_len的大小，出错时返回-1，并相应设置errno值。
+//2.这两个系统调用除了返回read,write返回的错误errno值，额外还会返回另外两种错误场景
+//第一个场景，若count个iov_len的和超出SSIZE_MAX，则不会处理任何数据，返回-1，并把errno值设置为EINVAL
+//第二个场景，count的数值必须大于0，且小于等于IOV_MAX，在LINUX中IOV_MAX的值为1024，若count为0，系统调用返回0。但是若count大于IOV_MAX，不会处理
+//任何数据，返回-1，并把errno设置为EINVAL
+
+//注：此处的count<=8时会使性能提升很多，内核栈会直接分配一个很小的段数组，避免了动态分配段内存
